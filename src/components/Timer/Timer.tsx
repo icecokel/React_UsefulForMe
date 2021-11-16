dff68e92915b82e003018f733514e150e60bb535
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2021-11-16 22:00:24 +0900

    Style : Add Css

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,7 +1,21 @@
 const Timer = (props: any) => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <h2>타이머</h2>
+      </div>
+      <div className="box_timer">
+        <div className="timer">시계부</div>
+        <div className="box_setting">
+          <div className="custom">커스텀 설정</div>
+          <div className="quick">빠른 설정</div>
+        </div>
+      </div>
+      <div>
+        <img src="images/timer.jpg" alt="tt" />
+      </div>
+    </div>
+  );
 };
-
-Timer.propTypes = {};
 
 export default Timer;
