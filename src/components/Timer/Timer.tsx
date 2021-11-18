0b7789ab45dc15bdf7b2c886c6b3817c79df15f5
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2021-11-18 14:54:02 +0900

    Feat : Add Timer

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -7,18 +7,36 @@ const SECOND_OF_DAY = 86400;
 
 const Timer = (props: any) => {
   const [sec, setSec] = useState<number>(SECOND_OF_HOUR);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
 
-  useEffect(() => {
-    console.log(sec);
-  }, [sec]);
+  useEffect(() => {}, [sec, isPlaying]);
   return (
     <div>
       <div>
         <h2>타이머</h2>
-        <PieTimer sec={sec} />
-      </div>
-      <div className="box_timer">
-        <div className="timer"></div>
+
+        <div className="box_timer">
+          <div className="timer">
+            <CountdownCircleTimer
+              isPlaying={isPlaying}
+              duration={sec}
+              size={500}
+              strokeWidth={50}
+              colors={[
+                ["#004777", 0.33],
+                ["#F7B801", 0.33],
+                ["#A30000", 0.33],
+              ]}
+            >
+              {({ remainingTime }: any) => {
+                if (!remainingTime) {
+                  return;
+                }
+                return Math.round((remainingTime / sec) * 100);
+              }}
+            </CountdownCircleTimer>
+          </div>
+        </div>
         <div className="box_setting">
           <div className="custom">커스텀 설정</div>
           <div className="quick">
@@ -26,6 +44,7 @@ const Timer = (props: any) => {
             <button
               onClick={() => {
                 setSec(SECOND_OF_HOUR);
+                setIsPlaying(true);
               }}
             >
               1시간
@@ -33,43 +52,26 @@ const Timer = (props: any) => {
             <button
               onClick={() => {
                 setSec(30 * SECOND_OF_MINUTE);
+                setIsPlaying(true);
               }}
             >
               30분
             </button>
+            <button
+              onClick={() => {
+                setSec(10 * SECOND_OF_MINUTE);
+                setIsPlaying(true);
+              }}
+            >
+              10분
+            </button>
           </div>
         </div>
       </div>
-      <div>
+      {/* <div>
         <img src="images/timer.jpg" alt="tt" />
-      </div>
+      </div> */}
     </div>
   );
 };
-
-const PieTimer = (props: any) => {
-  return (
-    <>
-      <div>
-        <CountdownCircleTimer
-          isPlaying
-          duration={props.sec}
-          colors={[
-            ["#004777", 0.33],
-            ["#F7B801", 0.33],
-            ["#A30000", 0.33],
-          ]}
-        >
-          {({ remainingTime }) => {
-            if (!remainingTime) {
-              return;
-            }
-            return Math.round((remainingTime / props.sec) * 100);
-          }}
-        </CountdownCircleTimer>
-      </div>
-    </>
-  );
-};
-
 export default Timer;
