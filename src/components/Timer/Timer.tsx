69dadd87f1c62bcf49f626cb2ca9455c8577d45d
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2021-11-20 18:27:14 +0900

    디자인 : 진행중

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -39,30 +39,28 @@ const Timer = (props: any) => {
   useEffect(() => {}, [sec, isPlaying]);
   return (
     <div>
-      <div>
-        <h2>타이머</h2>
+      <h2>타이머</h2>
 
-        <div className="box_timer">
-          <div className="timer">
-            <CountdownCircleTimer
-              isPlaying={isPlaying}
-              duration={sec}
-              size={500}
-              strokeWidth={50}
-              colors={[
-                ["#004777", 0.33],
-                ["#F7B801", 0.33],
-                ["#A30000", 0.33],
-              ]}
-            >
-              {({ remainingTime }: any) => {
-                if (!remainingTime) {
-                  return;
-                }
-                return Math.round((remainingTime / sec) * 100);
-              }}
-            </CountdownCircleTimer>
-          </div>
+      <div className="box_timer">
+        <div className="timer">
+          <CountdownCircleTimer
+            isPlaying={isPlaying}
+            duration={sec}
+            size={400}
+            strokeWidth={50}
+            colors={[
+              ["#004777", 0.33],
+              ["#F7B801", 0.33],
+              ["#A30000", 0.33],
+            ]}
+          >
+            {({ remainingTime }: any) => {
+              if (!remainingTime) {
+                return;
+              }
+              return Math.round((remainingTime / sec) * 100);
+            }}
+          </CountdownCircleTimer>
         </div>
         <div className="box_setting">
           <div className="custom">커스텀 설정</div>
