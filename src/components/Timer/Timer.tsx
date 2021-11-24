34fdbaf4a38dbe939c0fdbb205866445e0690f13
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2021-11-24 16:25:11 +0900

    완료 : 버그 수정   버그 - 커스텀 시간 설정 시 값이 없는 경우 에러 발생 완료 : 조작 버튼 기능 추가 완료 : 함수 주석 추가 완료 : 리팩토링

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -11,12 +11,100 @@ const quickSettingList = [
   { name: "10분", time: 10 * SECOND_OF_MINUTE },
 ];
 
+/**
+ * 타이머 컴포넌트
+ *
+ * @author LeeSangMin
+ * @since 2021/11/24
+ */
 const Timer = (props: any) => {
   const [sec, setSec] = useState<number>();
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [isEnableRestart, setIsEnableRestart] = useState<boolean>(false);
 
   useEffect(() => {}, [sec, isPlaying]);
 
+  /**
+   * 남은 시간 표시
+   *
+   * @param param 남은 시간 (초)
+   * @returns 남은 시간 (시, 분, 초)
+   */
+  const renderTime = ({ remainingTime }: any) => {
+    if (remainingTime === 0) {
+      return (
+        <div className="timer">
+          {isEnableRestart ? "1.5초 후 재시작 합니다." : "설정 대기중 .."}
+        </div>
+      );
+    }
+    const hour = Math.floor(remainingTime / SECOND_OF_HOUR);
+    const minute = Math.floor(
+      (remainingTime - SECOND_OF_HOUR * hour) / SECOND_OF_MINUTE
+    );
+    const second = Math.floor(remainingTime % SECOND_OF_MINUTE);
+
+    return (
+      <div className="timer">
+        <div className="text">남은 시간</div>
+        <div className="value">
+          {hour > 0 && hour + "시간 "}
+          {minute > 0 && minute + "분 "}
+          {second} 초
+        </div>
+      </div>
+    );
+  };
+
+  /**
+   * 커스텀 시간 설정
+   */
+  const onClickSetCutomerButton = () => {
+    const customHour = document.querySelector(
+      "#customHour"
+    ) as HTMLInputElement;
+    const customMinute = document.querySelector(
+      "#customMinute"
+    ) as HTMLInputElement;
+    const customSecond = document.querySelector(
+      "#customSecond"
+    ) as HTMLInputElement;
+
+    const customTime =
+      inputElementValueToNumber(customHour) * SECOND_OF_HOUR +
+      inputElementValueToNumber(customMinute) * SECOND_OF_MINUTE +
+      inputElementValueToNumber(customSecond);
+
+    setSec(customTime);
+    setIsPlaying(true);
+  };
+
+  /**
+   * 태그의 값의 자료형을 number로 변환
+   *
+   * @param target 인풋 태그
+   * @returns
+   * 자료형 number 인 태그의 값
+   * 태그의 값이 음수일 경우 양수로 반환
+   * 태그의 값이 없을 경우 0을 반환
+   */
+  const inputElementValueToNumber = (target: HTMLInputElement) => {
+    if (!target.value) {
+      return 0;
+    }
+
+    if (Number.parseInt(target.value) < 0) {
+      return Number.parseInt(target.value) * -1;
+    }
+
+    return Number.parseInt(target.value);
+  };
+
+  /**
+   * 빠른 설정 버튼 생성 메소드
+   *
+   * @returns 빠른 설정 버튼 컴포넌트
+   */
   const createQuickButton = () => {
     const buttonList = [] as any;
 
@@ -37,49 +125,6 @@ const Timer = (props: any) => {
 
     return buttonList;
   };
-
-  const renderTime = ({ remainingTime }: any) => {
-    if (remainingTime === 0) {
-      return <div className="timer">설정 중..</div>;
-    }
-    const hour = Math.floor(remainingTime / SECOND_OF_HOUR);
-    const minute = Math.floor(
-      (remainingTime - SECOND_OF_HOUR * hour) / SECOND_OF_MINUTE
-    );
-    const second = Math.floor(remainingTime % SECOND_OF_MINUTE);
-
-    return (
-      <div className="timer">
-        <div className="text">남은 시간</div>
-        <div className="value">
-          {hour > 0 && hour + "시간 "}
-          {minute > 0 && minute + "분 "}
-          {second} 초
-        </div>
-      </div>
-    );
-  };
-
-  const setCustomTime = () => {
-    const customHour = document.querySelector(
-      "#customHour"
-    ) as HTMLInputElement;
-    const customMinute = document.querySelector(
-      "#customMinute"
-    ) as HTMLInputElement;
-    const customSecond = document.querySelector(
-      "#customSecond"
-    ) as HTMLInputElement;
-
-    const customTime =
-      Number.parseInt(customHour.value) * SECOND_OF_HOUR +
-      Number.parseInt(customMinute.value) * SECOND_OF_MINUTE +
-      Number.parseInt(customSecond.value);
-
-    setSec(customTime);
-    setIsPlaying(true);
-  };
-
   return (
     <div>
       <h2>타이머</h2>
@@ -92,6 +137,9 @@ const Timer = (props: any) => {
             duration={sec ?? 0}
             size={500}
             strokeWidth={50}
+            onComplete={() => {
+              return [isEnableRestart, 1500];
+            }}
             colors={[
               ["#004777", 0.33],
               ["#F7B801", 0.33],
@@ -102,17 +150,25 @@ const Timer = (props: any) => {
           </CountdownCircleTimer>
         </div>
         <div className="box_setting">
-          <div className="">
+          <div className="box_contoller">
             <label>조작</label>
-            <button
-              onClick={() => {
-                setIsPlaying(!isPlaying);
-              }}
-            >
-              {isPlaying ? "일시정지" : "재개"}
-            </button>
+            <div>
+              <button
+                onClick={() => {
+                  setIsPlaying(!isPlaying);
+                }}
+              >
+                {isPlaying ? "일시정지" : "재개"}
+              </button>
 
-            <button>반복</button>
+              <button
+                onClick={() => {
+                  setIsEnableRestart(!isEnableRestart);
+                }}
+              >
+                {isEnableRestart ? "반복해제" : "반복"}
+              </button>
+            </div>
           </div>
           <div className="custom">
             <label>커스텀 설정</label>
@@ -120,7 +176,7 @@ const Timer = (props: any) => {
               <input type="number" id="customHour" />시
               <input type="number" id="customMinute" />분
               <input type="number" id="customSecond" />초
-              <button onClick={setCustomTime}>시작</button>
+              <button onClick={onClickSetCutomerButton}>시작</button>
             </div>
           </div>
           <div className="quick">
