9f08eef330de36bbc7bf02c106d4c1f8f83c9b6c
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2021-11-26 14:55:47 +0900

    준비 : 메인 컴포넌트 디자인

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,44 +1,39 @@
-import { useState } from "react";
-import { PagePath } from "../common/enum";
+import PageInfo from "../common/PageInfoUtil";
 
+/**
+ * 메인 컴포넌트
+ *
+ * @author LeeSangMin
+ * @since 2021/11/24
+ */
 const Main = (props: any) => {
-  const bubbleList = [
-    { title: "Timer", path: PagePath.Timer },
-    { title: "wordQuiz", path: PagePath.WordQuiz },
-    { title: "memo", path: PagePath.Memo },
-  ];
-
-  const displayBubble = () => {
-    console.log("2323");
-
-    const bubbles = [] as any;
-
-    if (!bubbleList) {
-      return;
-    }
-
-    bubbleList.forEach((item) => {
-      bubbles.push(<Bubble title={item.title} path={item.path} />);
-    });
-
-    return bubbles;
-  };
-
   return (
     <div>
-      <div>{displayBubble()}</div>
-    </div>
-  );
-};
-const Bubble = (props: any) => {
-  return (
-    <div>
-      <div>
-        <label htmlFor={props.title + "_link"}>
-          <a id={props.title + "_link"} href={props.path}>
-            {props.title}
-          </a>
-        </label>
+      <h2>메인</h2>
+      <div className="box_main">
+        <div className="box_dashboard">
+          <h4>대시보드</h4>
+          <div>
+            <h1>구상중</h1>
+          </div>
+        </div>
+        <div className="box_menu">
+          <ul>
+            {PageInfo.getPageInfo().map((item) => {
+              return (
+                <li>
+                  <button
+                    onClick={() => {
+                      window.location.href = item.path;
+                    }}
+                  >
+                    {item.title}
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </div>
   );
