e7352dda18047eb96668c52bb8510b716d5abf31
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2021-11-30 00:10:02 +0900

    완료 : 메인 이동 버튼 기

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,12 +1,16 @@
-import React from "react";
-
+import { useHistory } from "react-router";
+import { PagePath } from "../common/enum";
 const Error = () => {
+  const history = useHistory();
+  const goToMain = () => {
+    history.push(PagePath.Main);
+  };
   return (
     <div className="box_error">
       <div>
         <h2>!! 404 에러 !!</h2>
         <p>페이지를 찾을 수 없습니다.</p>
-        <button>메인으로 가기</button>
+        <button onClick={goToMain}>메인으로 가기</button>
       </div>
     </div>
   );
