f088946569d414a1ad7709bf1a02bf3bbbca36d2
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2021-11-29 14:37:26 +0900

    완료 : 404 페이지 추가

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <div className="box_error">
+      <div>
+        <h2>!! 404 에러 !!</h2>
+        <p>페이지를 찾을 수 없습니다.</p>
+        <button>메인으로 가기</button>
+      </div>
+    </div>
+  );
+};
+
+export default Error;
