4747575587aede360291e01680285f7962a63709
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2021-12-09 19:32:13 +0900

    진행 : 보드 게임 컴포넌트 생성

---
+++ src/components/BoardGame/BoardGame.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../Header";
+
+const BoardGame = (props: any) => {
+  return (
+    <div>
+      <div>
+        <Header />
+      </div>
+      <h2>보드게임 - 추리왕</h2>
+      <div></div>
+    </div>
+  );
+};
+
+export default BoardGame;
