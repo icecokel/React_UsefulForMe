654be0b6cc6801d8ae9f56ba2aa4da62817ca13b
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2021-12-10 13:11:14 +0900

    완료 : 보드게임 기본 데이터 작성

--- src/common/enum.ts
+++ src/common/enum.ts
@@ -7,3 +7,33 @@ export enum PagePath {
   TodoList = "/TodoList",
   BoardGame = "/BoardGame",
 }
+
+export enum suspectList {
+  carRacer = "카레이서",
+  reporter = "여기자",
+  ceo = "K그룹 회장",
+  boxer = "복싱선수",
+  proGamer = "프로게이머",
+  carsinoOwner = "카지노 사장",
+}
+
+export enum placeList {
+  parkinglot = "주차장",
+  hetel = "호텔",
+  casino = "카지노",
+  pension = "펜션",
+  cafe = "커피숍",
+  movie = "극장",
+  restaurant = "레스토랑",
+  convenienceStore = "편의점",
+  park = "공원",
+}
+
+export enum toolList {
+  poison = "독약",
+  hammer = "망치",
+  gun = "권총",
+  banana = "바나나 껍질",
+  knife = "칼",
+  flowerPot = "화분",
+}
