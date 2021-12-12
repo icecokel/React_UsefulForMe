0685d3e8cb1cf645a02e37db42ae96c0d17d3bc9
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2021-12-12 15:33:11 +0900

    진행 : 이넘 리스트화

--- src/components/BoardGame/BoardGame.tsx
+++ src/components/BoardGame/BoardGame.tsx
@@ -1,5 +1,34 @@
-import React from "react";
 import Header from "../Header";
+
+const SuspectList = [
+  { carRacer: "카레이서" },
+  { reporter: "여기자" },
+  { ceo: "K그룹 회장" },
+  { boxer: "복싱선수" },
+  { proGamer: "프로게이머" },
+  { carsinoOwner: "카지노 사장" },
+];
+
+const PlaceList = [
+  { parkinglot: "주차장" },
+  { hetel: "호텔" },
+  { casino: "카지노" },
+  { pension: "펜션" },
+  { cafe: "커피숍" },
+  { movie: "극장" },
+  { restaurant: "레스토랑" },
+  { convenienceStore: "편의점" },
+  { park: "공원" },
+];
+
+const ToolList = [
+  { poison: "독약" },
+  { hammer: "망치" },
+  { gun: "권총" },
+  { banana: "바나나 껍질" },
+  { knife: "칼" },
+  { flowerPot: "화분" },
+];
 
 const BoardGame = (props: any) => {
   return (
