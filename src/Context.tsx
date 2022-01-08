02729a1b9e321b50fc042cf434226970e84e5070
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-01-08 17:00:29 +0900

    완료 : 메모를 할 일 목록 으로 수정

--- src/Context.tsx
+++ src/Context.tsx
@@ -10,7 +10,7 @@ type State = {
 };
 
 // 모든 액션들을 위한 타입
-type Action = { type: "SET_MEMO_COUNT"; memoCount: number };
+type Action = { type: "SET_TODO_COUNT"; todoCount: number };
 
 // 디스패치를 위한 타입 (Dispatch 를 리액트에서 불러올 수 있음), 액션들의 타입을 Dispatch 의 Generics로 설정
 // Context 만들기
@@ -20,10 +20,10 @@ const DispatchContext = createContext<Dispatch<Action> | null>(null);
 // 리듀서
 function reducer(state: State, action: Action): State {
   switch (action.type) {
-    case "SET_MEMO_COUNT":
+    case "SET_TODO_COUNT":
       return {
         ...state,
-        memoCount: action.memoCount, // count가 자동완성되며, number 타입인걸 알 수 있습니다.
+        memoCount: action.todoCount, // count가 자동완성되며, number 타입인걸 알 수 있습니다.
       };
 
     default:
