ffbd048a275f581338ab69876eb28baa420efe8c
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-01-10 20:56:43 +0900

    진행 : 테트리스 추가

---
+++ src/components/Tetris/Tetris.tsx
@@ -0,0 +1,14 @@
+import Header from "../Header";
+
+const Tetris = (props: any) => {
+  return (
+    <div>
+      <div>
+        <Header />
+      </div>
+      <h2>게임 - 테트리스</h2>
+    </div>
+  );
+};
+
+export default Tetris;
