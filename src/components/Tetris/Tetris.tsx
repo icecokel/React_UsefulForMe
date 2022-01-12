109260f397bbb182c4cd6a53fdfc9e725210ca49
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-01-12 23:29:13 +0900

    진행 : 테트리스 구조

--- src/components/Tetris/Tetris.tsx
+++ src/components/Tetris/Tetris.tsx
@@ -7,6 +7,11 @@ const Tetris = (props: any) => {
         <Header />
       </div>
       <h2>게임 - 테트리스</h2>
+      <div>
+        <div>스코어</div>
+        <div>게임 메인</div>
+        <div>넥스트 블록</div>
+      </div>
     </div>
   );
 };
