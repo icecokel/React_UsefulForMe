11e485a59a7dd68b3ab0699c5040ab7aee5dcc32
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-01-12 23:47:11 +0900

    진행 : 구조

--- src/components/Tetris/Tetris.tsx
+++ src/components/Tetris/Tetris.tsx
@@ -8,12 +8,20 @@ const Tetris = (props: any) => {
       </div>
       <h2>게임 - 테트리스</h2>
       <div>
-        <div>스코어</div>
+        <ScoreBoard />
         <div>게임 메인</div>
-        <div>넥스트 블록</div>
+        <NextBlock />
       </div>
     </div>
   );
+};
+
+const ScoreBoard = (props: any) => {
+  return <div>스코어 보드</div>;
+};
+
+const NextBlock = (props: any) => {
+  return <div>넥스트 블록</div>;
 };
 
 export default Tetris;
