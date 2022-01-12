ead54f8dd58635b2d3d8098243341004a70afccf
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-01-12 23:56:28 +0900

    진행 : 블록 생성 로직

--- src/components/Tetris/Tetris.tsx
+++ src/components/Tetris/Tetris.tsx
@@ -1,12 +1,18 @@
 import Header from "../Header";
 
+const BlockList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 const Tetris = (props: any) => {
+  const createBlock = () => {
+    console.log(Math.random());
+  };
   return (
     <div>
       <div>
         <Header />
       </div>
       <h2>게임 - 테트리스</h2>
+      <button onClick={createBlock}>111</button>
       <div>
         <ScoreBoard />
         <div>게임 메인</div>
@@ -22,6 +28,10 @@ const ScoreBoard = (props: any) => {
 
 const NextBlock = (props: any) => {
   return <div>넥스트 블록</div>;
+};
+
+const Block = (kind: number) => {
+  return <div></div>;
 };
 
 export default Tetris;
