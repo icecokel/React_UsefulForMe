c0e1cb3f20533b4f2c23c51687277d83e9cb6d8c
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-01-21 15:03:18 +0900

    진행 : 새로운 메모 만들기

--- src/components/Memo/Memo.tsx
+++ src/components/Memo/Memo.tsx
@@ -2,6 +2,13 @@ import Header from "../Header";
 import FirebaseService from "../../common/FirebaseService";
 import { useEffect, useState } from "react";
 
+interface MEMO {
+  title: string;
+  contents: string;
+  createAt: any;
+  updateAt: any;
+}
+
 /**
  * 메모 컴포넌트
  *
@@ -9,12 +16,11 @@ import { useEffect, useState } from "react";
  * @since 2021/12/05
  */
 const Memo = (props: any) => {
-  const [memoList, setMemoList] = useState<Array<any>>();
-  const [currentMemo, setCurrentMemo] = useState<any>();
+  const [memoList, setMemoList] = useState<Array<MEMO>>();
+  const [currentMemo, setCurrentMemo] = useState<MEMO>();
 
   useEffect(() => {
     !memoList && setMemo();
-    console.log(currentMemo.createAt.toDate());
   });
 
   const setMemo = async () => {
@@ -27,6 +33,17 @@ const Memo = (props: any) => {
     setMemoList(tempList);
   };
 
+  const createNewMemo = () => {
+    const newMemo: MEMO = {
+      title: "",
+      contents: "",
+      createAt: undefined,
+      updateAt: undefined,
+    };
+
+    setCurrentMemo(newMemo);
+  };
+
   const formatTime = (time: Date) => {
     return `
     ${time.getFullYear().toString().slice(-2)}/
@@ -35,7 +52,6 @@ const Memo = (props: any) => {
     )} ${("0" + time.getHours()).slice(-2)}:${("0" + time.getMinutes()).slice(
       -2
     )}`;
-    // ("0" + time.getMonth()).slice(-2)
   };
   return (
     <div>
@@ -45,13 +61,13 @@ const Memo = (props: any) => {
       <h2>메모</h2>
       <div className="box_memo">
         <div className="box_title">
-          <label>새로운 메모 만들기</label>
+          <label onClick={createNewMemo}>새로운 메모 만들기</label>
           <ol>
             {memoList?.map((memo) => {
               return (
                 <li
                   className={
-                    memo.title === currentMemo.title
+                    currentMemo && memo.title === currentMemo.title
                       ? "item-title-selected"
                       : ""
                   }
@@ -68,17 +84,34 @@ const Memo = (props: any) => {
         {currentMemo && (
           <div className="box_contents">
             <div className="box_memo_title">
-              <input type="text" value={currentMemo.title} />
+              <input
+                type="text"
+                value={currentMemo.title}
+                placeholder="제목을 입력해주세요."
+              />
               <div>
-                <span>작성일 :{formatTime(currentMemo.createAt.toDate())}</span>
+                <span>
+                  작성일 :
+                  {currentMemo.createAt
+                    ? formatTime(currentMemo.createAt.toDate())
+                    : ""}
+                </span>
                 <br />
-                <span>수정일 :{formatTime(currentMemo.updateAt.toDate())}</span>
+                <span>
+                  수정일 :
+                  {currentMemo.updateAt
+                    ? formatTime(currentMemo.updateAt.toDate())
+                    : ""}
+                </span>
               </div>
             </div>
             <hr />
-            <textarea cols={100} rows={50}>
-              {currentMemo.contents}
-            </textarea>
+            <textarea
+              cols={100}
+              rows={50}
+              value={currentMemo.contents}
+              placeholder="내용을 입력해주세요."
+            ></textarea>
             <hr />
             <div>
               <button>저장</button>
