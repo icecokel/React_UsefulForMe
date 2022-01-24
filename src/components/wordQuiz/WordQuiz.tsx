cddbbdba684609a435fd17ca8cf5d4b22022beba
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-01-24 20:24:05 +0900

    진행 : 단어퀴즈 컴포넌트

--- src/components/wordQuiz/WordQuiz.tsx
+++ src/components/wordQuiz/WordQuiz.tsx
@@ -1,5 +1,7 @@
-import React from "react";
 import Header from "../Header";
+import FirebaseService from "../../common/FirebaseService";
+
+const LENGUAGE_LIST = ["word_jp"];
 
 /**
  * 단어퀴즈 컴포넌트
@@ -8,13 +10,26 @@ import Header from "../Header";
  * @since 2021/11/30
  */
 const WordQuiz = (props: any) => {
+  const fetchWords = async (lenguageIndex: number) => {
+    const result = await FirebaseService.fetchWordQuiz(
+      LENGUAGE_LIST[lenguageIndex]
+    );
+
+    console.log(result);
+  };
   return (
     <div>
       <div>
         <Header />
       </div>
       <h2>단어 퀴즈</h2>
-      <div className="box_wordQuiz"></div>
+      <div className="box_wordQuiz">
+        <div>
+          <h4>언어</h4>
+          <button onClick={() => fetchWords(0)}>일어</button>
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 };
