76ae6748c1ffa500c54f973b655106957ba3d342
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-01-30 23:25:32 +0900

    진행 : 구조짜는중

--- src/components/Tetris/Tetris.tsx
+++ src/components/Tetris/Tetris.tsx
@@ -1,29 +1,25 @@
 import { useState, useRef } from "react";
 import Header from "../Header";
+import GemeHelper from "./GameHelper";
 
 const BLOCK_LIST = ["J", "L", "S", "O", "Z", "T", "I"];
 const MIN_VALUE = 0;
 const MAX_VALUE = 6;
 
-const BOARD_COLUMNS = 12;
-const BOARD_ROWS = 20;
+// const BOARD_COLUMNS = 12;
+// const BOARD_ROWS = 20;
 
 const Tetris = (props: any) => {
   const [nextBlock, setNextBlock] = useState<Array<string>>([]);
   const [gameStatus, setGameState] = useState<string>("Ready");
-  const triggerRef = useRef<HTMLInputElement>(null);
 
+  GemeHelper.moveBlock(false);
   const createBlock = () => {
     const blockIndex = Math.floor(Math.random() * MAX_VALUE + MIN_VALUE);
     return <Block kind={BLOCK_LIST[blockIndex]}></Block>;
   };
 
   const onClickStart = () => {
-    if (!triggerRef) {
-      return;
-    }
-
-    triggerRef.current?.focus();
     setGameState("Start");
   };
 
@@ -32,10 +28,10 @@ const Tetris = (props: any) => {
       <div>
         <Header />
       </div>
-      <h2>게임 - 테트리스</h2>
+      <h2>게임 - 테트리스 {gameStatus}</h2>
+
       <button onClick={createBlock}>테스트</button>
       <button onClick={onClickStart}> Start</button>
-      <input type="text" ref={triggerRef} hidden />
       <div>
         <ScoreBoard />
         <div>게임 메인</div>
